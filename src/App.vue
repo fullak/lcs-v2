efd83edc880fd446a5390a276c65197c0dd04760
tristan brochard <brochardtristan@gmail.com>
2021-01-11 12:25:33 +0100

    init project + add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="flex flex-row">
+      <router-link to="/" class="navigation-link">A propos</router-link> |
+      <router-link to="" class="navigation-link">Formations</router-link> |
+      <router-link to="" class="navigation-link">Projets</router-link> |
+      <router-link to="" class="navigation-link">CDB</router-link>
+
+      <div class="absolute top-0 right-0 flex flex-row">
+        <button class="navigation-btn" @click="isVisible = !isVisible" v-show="!isVisible">Follow me!</button>
+        <a href="https://www.instagram.com/learn_computer_science/" target="_blank">
+          <img src="../src/assets/img/instagram.png" alt="" class="px-2 h-10 mt-6" v-show="isVisible" >
+        </a>
+        <a href="https://www.youtube.com/channel/UCPsR5Z9laQtVkKy728gZQIQ?view_as=subscriber" target="_blank">
+          <img src="../src/assets/img/youtube.png" alt="" class="px-2 h-10 mt-6" v-show="isVisible">
+        </a>
+        <router-link to="/">
+          <img src="../src/assets/img/mainleve.png" alt="" class="h-20 px-2 py-2">
+        </router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      isVisible: false,
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+html, body {
+  background-color: #152634;
+}
+
+@font-face {
+  font-family: "JetBrains Mono";
+  src: url("/assets/Font/JetBrainsMono.ttf");
+}
+
+@font-face {
+  font-family: "zerohour";
+  src: url("/assets/Font/zerohour.ttf");
+}
+
+.navigation-link {
+  @apply text-white hover:text-black hover:bg-white rounded px-2 py-0.5;
+}
+
+.navigation-btn {
+  @apply text-white px-2 h-10 mt-6 border border-white rounded mr-5 hover:text-black hover:bg-white;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #2c3e50; //primary color
 }
 
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: #101E2A; //secondary color
+  font-family: "JetBrains Mono", sans-serif;
 }
 </style>
